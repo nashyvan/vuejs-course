f2ff0679c769ccf9208e82ab0a9fe3fba437ccd2
nashivan <yarik.nashivan@gmail.com>
2021-11-13 18:13:56 +0200

    axios and sorting

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,40 @@
 <template>
   <div class="app">
     <h1>Page with posts</h1>
-    <my-button class="my-15px" @click="showDialog">Create post</my-button>
+    <div class="app__buttons">
+      <my-button class="my-15px" @click="showDialog">Create post</my-button>
+      <my-select :options="sortOptions" v-model="selectedSort"/>
+    </div>
     <my-dialog v-model:show="dialogVisible">
       <post-form @create="createPost"/>
     </my-dialog>
-    <post-list :posts="posts" @remove="removePost" />
+    <post-list :posts="sortedPosts" @remove="removePost" v-if="!isPostsLoading" />
+    <div v-else>Loading...</div>
   </div>
 </template>
 
 <script>
-import PostForm from "@/components/PostForm";
-import PostList from "@/components/PostList";
+import axios from 'axios'
+
+import PostForm from "./components/PostForm";
+import PostList from "./components/PostList";
+import MyButton from "./components/UI/MyButton";
+import MySelect from "./components/UI/MySelect";
 
 export default {
   components: {
-    PostForm, PostList,
+    PostForm, PostList, MyButton, MySelect
   },
   data() {
     return {
-      posts: [
-        {id: 1, title: 'JavaScript', body: 'Description'},
-        {id: 2, title: 'JavaScript 2', body: 'Description 2'},
-        {id: 3, title: 'JavaScript 3', body: 'Description 3'},
-        {id: 4, title: 'JavaScript 4', body: 'Description 4'},
-      ],
+      posts: [],
       dialogVisible: false,
+      isPostsLoading: false,
+      selectedSort: '',
+      sortOptions: [
+        { value: 'title', name: 'Sort by title' },
+        { value: 'body', name: 'Sort by body' },
+      ]
     }
   },
   methods: {
@@ -39,7 +48,33 @@ export default {
     showDialog() {
       this.dialogVisible = true
     },
-  }
+    async fetchPosts() {
+      try {
+        this.isPostsLoading = true
+        const response = await axios.get('https://jsonplaceholder.typicode.com/posts?_limit=10')
+        this.posts = response.data
+      } catch (e) {
+        alert('Error')
+      } finally {
+        this.isPostsLoading = false
+      }
+    }
+  },
+  mounted() {
+    this.fetchPosts()
+  },
+  computed: {
+    sortedPosts() {
+      return [...this.posts].sort((post1, post2) => post1[this.selectedSort]?.localeCompare(post2[this.selectedSort]))
+    },
+  },
+  // watch: {
+  //   selectedSort(newValue) {
+  //     this.posts.sort((post1, post2) => {
+  //       return post1[newValue]?.localeCompare(post2[newValue])
+  //     })
+  //   }
+  // }
 }
 </script>
 
@@ -53,6 +88,11 @@ export default {
 
 .app {
   padding: 20px;
+}
+
+.app__buttons {
+  display: flex;
+  justify-content: space-between;
 }
 
 .self-end {
--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -1,7 +1,9 @@
 <template>
   <div v-if="posts.length > 0">
     <h3>Posts list</h3>
-    <post-item v-for="post in posts" :post="post" :key="post.id" @remove="$emit('remove', post)"/>
+    <transition-group name="post-list">
+      <post-item v-for="post in posts" :post="post" :key="post.id" @remove="$emit('remove', post)"/>
+    </transition-group>
   </div>
   <h2 v-else>
     No posts yet
@@ -22,7 +24,18 @@ export default {
 </script>
 
 <style scoped>
-
-
-
+.post-list-item {
+  display: inline-block;
+  margin-right: 10px;
+}
+.post-list-enter-active, .post-list-leave-active {
+  transition: all 0.4s ease;
+}
+.post-list-enter, .post-list-leave-to {
+  opacity: 0;
+  transform: translateX(30px);
+}
+.post-list-move {
+  transition: transform 0.4s ease;
+}
 </style>
