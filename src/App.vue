4e91206e03be530580cd73b5a7dc793a2fab900e
nashivan <yarik.nashivan@gmail.com>
2021-11-14 14:26:06 +0200

    load more posts

--- src/App.vue
+++ src/App.vue
@@ -11,9 +11,10 @@
     </my-dialog>
     <post-list :posts="sortedAndSearchedPosts" @remove="removePost" v-if="!isPostsLoading" />
     <div v-else>Loading...</div>
-    <div class="page__wrapper">
-      <div class="page" v-for="pageNumber in totalPages" :key="pageNumber" :class="{'current-page': page === pageNumber}" @click="changePage(pageNumber)">{{ pageNumber }}</div>
-    </div>
+    <div ref="observer" class="observer"></div>
+<!--    <div class="page__wrapper">-->
+<!--      <div class="page" v-for="pageNumber in totalPages" :key="pageNumber" :class="{'current-page': page === pageNumber}" @click="changePage(pageNumber)">{{ pageNumber }}</div>-->
+<!--    </div>-->
   </div>
 </template>
 
@@ -56,6 +57,9 @@ export default {
     showDialog() {
       this.dialogVisible = true
     },
+    // changePage(pageNumber) {
+    //   this.page = pageNumber
+    // },
     async fetchPosts() {
       try {
         this.isPostsLoading = true
@@ -73,12 +77,36 @@ export default {
         this.isPostsLoading = false
       }
     },
-    changePage(pageNumber) {
-      this.page = pageNumber
+    async loadMorePosts() {
+      try {
+        this.page += 1
+        const response = await axios.get('https://jsonplaceholder.typicode.com/posts', {
+          params: {
+            _page: this.page,
+            _limit: this.limit,
+          }
+        })
+        this.totalPages = Math.ceil(response.headers['x-total-count'] / this.limit)
+        this.posts = [...this.posts, ...response.data]
+      } catch (e) {
+        alert('Error')
+      }
     },
   },
   mounted() {
     this.fetchPosts()
+    console.log(this.$refs.observer)
+    let options = {
+      rootMargin: '0px',
+      threshold: 1.0
+    }
+    let callback = (entries, observer) => {
+      if (entries[0]. isIntersecting && this.page < this.totalPages) {
+        this.loadMorePosts()
+      }
+    }
+    let observer = new IntersectionObserver(callback, options)
+    observer.observe(this.$refs.observer )
   },
   computed: {
     sortedPosts() {
@@ -89,9 +117,9 @@ export default {
     }
   },
   watch: {
-    page() {
-      this.fetchPosts()
-    }
+    // page() {
+    //   this.fetchPosts()
+    // }
   }
 }
 </script>
@@ -138,6 +166,11 @@ export default {
 
 .current-page {
   border: 2px solid teal;
+}
+
+.observer {
+  height: 30px;
+  background: gray;
 }
 
 </style>
